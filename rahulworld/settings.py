579e2dd4487ef1685041fa8f6ec55ac9d2263fdc
akash <akashsen22@gmail.com>
2023-04-17 12:56:48 +0530

    third

--- rahulworld/settings.py
+++ rahulworld/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 from whitenoise import WhiteNoise
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x3zs0@4l&+@+2rm(i1@2xlwlj3-jm((qc=)#uv*oqcumsz+mu8'
+SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG")
 
 ALLOWED_HOSTS = ['*']
 
@@ -80,14 +81,11 @@ WSGI_APPLICATION = 'rahulworld.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'RAHULWORLD',
-        'USER': 'rahulworld',
-        'PASSWORD':'rahulworld',
-        'HOST': 'rahulworld.cdpvuhwajzhq.ap-south-1.rds.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -123,8 +121,8 @@ USE_TZ = True
 # Email Configuration
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'rahulworld74@gmail.com'
-EMAIL_HOST_PASSWORD ='tahvqbgqvbjnezen'
+EMAIL_HOST_USER = config('EMAIL_HOST', default='localhost')
+EMAIL_HOST_PASSWORD =config('EMAIL_PORT', default=25, cast=int)
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'rahulworld<rahulworld74@gmail.com>'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
@@ -146,12 +144,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-# AWS_ACCESS_KEY_ID = 'AWS_ACCESS_KEY_ID '
-# AWS_SECRET_ACCESS_KEY = 'AWS_SECRET_ACCESS_KEY'
-# AWS_STORAGE_BUCKET_NAME = 'AWS_STORAGE_BUCKET_NAME'
-# AWS_S3_SIGNATURE_NAME = 's3v4',
-# AWS_S3_REGION_NAME = 'AWS_S3_REGION_NAME'
-# AWS_S3_FILE_OVERWRITE = False
-# AWS_DEFAULT_ACL =  None
-# AWS_S3_VERITY = True
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
